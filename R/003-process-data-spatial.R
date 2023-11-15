caf35958e18c9af8528e7b10f766d952a93a5a45
edwardlavender <el72@st-andrews.ac.uk>
2023-11-15 22:17:21 +0100

    Add analysis of bathymetry data

--- R/003-process-data-spatial.R
+++ R/003-process-data-spatial.R
@@ -103,10 +103,73 @@ digi <- abs(digi)
 digi <- terra::project(digi, terra::crs(bathy))
 digi <- terra::resample(digi, bathy, method = "near")
 # Update NAs on bathy with values from digi
+howe <- bathy
 mask <- is.na(bathy)
 bathy[mask] <- digi[mask]
 terra::plot(bathy)
 toc()
+
+
+###########################
+###########################
+#### Analyse bathymetry layers
+
+# Examine the extent of howe on digi
+terra::plot(digi)
+terra::plot(howe, col = "dimgrey", add = TRUE)
+# In regions with data, estimate additional error induced by digi
+# * We need to use this information in the depth error model
+error <- howe - digi
+error <- terra::mask(error, bathy)
+# Most errors are small
+# * 95 % percentiles: -4.39 m to 7.19 m
+terra::hist(error, maxcell = 1e9) 
+terra::global(error, quantile, probs = c(0.025, 0.5, 0.975), na.rm = TRUE)
+terra::global(error, "mean", na.rm = TRUE) # 0.5 m (small)
+# There are some places where Howe is shallower than Digi
+terra::plot(error < -50)
+terra::global(error, "min", na.rm = TRUE) 
+# There are some areas (coastline) where Howe is MUCH deeper than digi
+# * This makes sense given lower resolution of Digi. 
+terra::global(error, "max", na.rm = TRUE)  # 349 m (huge!)
+# Examine the error with depth 
+# * Most errors are 'small'
+# * Small errors occur across the full range of depths
+# * The largest errors are only found in deep water (this makes sense)
+n <- 1e6
+mf <- terra::spatSample(error, size = n, 
+                        method = "random", replace = FALSE, na.rm = TRUE,
+                        values = TRUE, cell = TRUE, as.df = TRUE)
+mf$bathy <- terra::extract(howe, mf$cell)[, 1]
+colnames(mf) <- c("cell", "error", "howe")
+plot(mf$error, mf$howe, pch = ".")
+# Examine errors in the region around Lismore
+# * This is the most important region where we lack Howe data
+# locator()
+south_lismore <- terra::ext(705117.5, 719807.8, 
+                            6258559, 6271233)
+if (FALSE) {
+  # Xoom in further:
+  south_lismore <- terra::ext(707696.4, 710613.7, 
+                              6266425, 6269443)
+}
+howe_lismore <- terra::crop(howe, south_lismore)
+digi_lismore <- terra::crop(digi, south_lismore)
+# There is no noticeable jump at the boundary
+# * But this map is relatively large in scale
+terra::plot(digi_lismore, range = c(0, 250))
+terra::plot(howe_lismore, range = c(0, 250), add = TRUE)
+terra::sbar(1e3)
+# Depths are +/- ~40 m in this region
+# * I.e., if an individual moves into this region, 
+# * the influence of the depth observations should become weaker
+# * That being said, the values are still mostly similar & 
+# * ... there is not a strong spatial pattern
+error_lismore <- howe_lismore - digi_lismore
+terra::global(error_lismore, quantile, na.rm = TRUE)
+terra::plot(error_lismore)
+terra::plot(error_lismore > 10)
+terra::plot(error_lismore < -10)
 
 
 ###########################
@@ -148,6 +211,7 @@ toc()
 tic()
 saveRDS(mpa, here_data("spatial", "mpa.rds"))
 saveRDS(coast, here_data("spatial", "coast.rds"))
+terra::writeRaster(howe, here_data("spatial", "howe.tif"), overwrite = TRUE)
 terra::writeRaster(bathy, here_data("spatial", "bathy.tif"), overwrite = TRUE)
 toc()
 
