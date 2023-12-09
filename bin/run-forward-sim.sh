72b81aff0f8e44a9e34074275ea327a0c0e8916c
edwardlavender <el72@st-andrews.ac.uk>
2023-12-09 10:41:17 +0100

    Add bin folder

---
+++ bin/run-forward-sim.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Instructions to run the forward simulation on siam-linux20
+
+# 1. Copy RStudio Project onto server into documents/projects/patter-flapper
+# 2. ssh into server
+ssh lavended@siam-linux20 
+
+# 3. Open RStudio Project interactively to create project library via {renv}
+cd documents/projects/patter-flapper/
+R
+q("no")
+
+# 3. Run the forward simulation
+R CMD BATCH R/010-run-forward-sim.R >>> data/output/forward/log.txt
