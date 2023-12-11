3e9a3c048542cafd4e2ff6be63330c145b024855
edwardlavender <el72@st-andrews.ac.uk>
2023-12-11 17:49:30 +0100

    Update bin

--- bin/run-forward-sim.sh
+++ bin/run-forward-sim.sh
@@ -3,11 +3,16 @@
 # Instructions to run the forward simulation on siam-linux20
 
 # 1. Copy RStudio Project onto server into documents/projects/patter-flapper
+
 # 2. ssh into server
 ssh lavended@siam-linux20 
 
+# 3. Update code
+cd documents/projects
+git pull https://github.com/edwardlavender/patter-flapper.git
+
 # 3. Open RStudio Project interactively to create project library via {renv}
-cd documents/projects/patter-flapper/
+cd patter-flapper
 R
 q("no")
 
