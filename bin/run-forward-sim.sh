57461dd9333ed1e244b14e897b3f229124bd36a8
edwardlavender <el72@st-andrews.ac.uk>
2023-12-11 18:22:10 +0100

    Include SIA-LAVENDED-M test in bash code

--- bin/run-forward-sim.sh
+++ bin/run-forward-sim.sh
@@ -2,6 +2,16 @@
 
 # Instructions to run the forward simulation on siam-linux20
 
+# ---------- SIA-LAVENDED-M test ---------- 
+
+# 1. Navigate to RStudio Project
+cd /Users/lavended/Documents/work/packages/flapper/patter-flapper/patter-flapper/
+
+# 2. Run code 
+R CMD BATCH ./R/010-run-forward-sim-1.R ./data/output/forward/log.txt
+
+# ---------- siam-linux20 ---------- 
+
 # 1. Copy RStudio Project onto server into documents/projects/patter-flapper
 
 # 2. ssh into server
@@ -14,7 +24,13 @@ git pull https://github.com/edwardlavender/patter-flapper.git
 # 3. Open RStudio Project interactively to create project library via {renv}
 cd patter-flapper
 R
+Sys.setenv(GITHUB_PAT = "<insert-pat-here>")
+t1 <- Sys.time()
+renv::restore()
+t2 <- Sys.time()
+difftime(t2, t1)
 q("no")
 
 # 3. Run the forward simulation
-R CMD BATCH R/010-run-forward-sim.R >>> data/output/forward/log.txt
+
+R CMD BATCH ./R/010-run-forward-sim-1.R ./data/output/forward/log.txt
