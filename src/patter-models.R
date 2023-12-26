31dda518adb4788c771e7c6a5f20848a2a804cdf
edwardlavender <el72@st-andrews.ac.uk>
2023-12-26 18:04:42 +0000

    Update depth likelihood function for revised patter routines

--- src/patter-models.R
+++ src/patter-models.R
@@ -12,37 +12,46 @@ dc_origin <- function(.bathy, .depth, .calc_depth_error) {
   terra::clamp(.bathy, shallow, deep, values = FALSE)
 }
 
-#' @title Depth weighting function
+#' @title Depth likelihood term
 
-update_ac <- function(.particles, .bathy, .obs, .t, .bset, .calc_depth_error) {
+pf_lik_dc_2 <- function(.particles, .obs, .t, .dlist) {
+  # Checks (one off)
+  if (.t == 1L) {
+    patter:::check_dlist(.dlist = .dlist, 
+                         .spatial = "bathy", 
+                         .algorithm = c("bset", "calc_depth_error"))
+  }
   # Extract depth of seabed at particle positions
   cell_now <- NULL
   if (!rlang::has_name(.particles, "bathy")) {
-    .particles[, bathy := terra::extract(.bathy, cell_now)]
+    .particles[, bathy := terra::extract(.dlist$spatial$bathy, cell_now)]
   }
   # Define bathymetry data source (digi versus howe) & define adjustment for depth-error model
   # > For some receivers in the Howe data, the individual is ~10-20 m shallower than previously assumed.
   # > For the Digimap data, the individual can be up to 40 m shallower. 
-  .particles[, digi := terra::extract(.bset, cell_now)]
+  .particles[, digi := terra::extract(.dlist$algorithm$bset, cell_now)]
   .particles[, boost := 20L]
   bool_digi <- .particles$digi == 1L
   if (any(.particles$digi == 1L, na.rm = TRUE)) {
     .particles[which(bool_digi), boost := 40L]
   }
   # Define shallow and deep depth limits for each particle based on location
-  .particles[, error := .calc_depth_error(.obs$depth[.t])]
+  .particles[, error := .dlist$algorithm$calc_depth_error(.obs$depth[.t])]
   .particles[, deep := .obs$depth[.t] + error]
   .particles[, shallow_1 := .obs$depth[.t] - (error + 5)]
   .particles[, shallow_2 := .obs$depth[.t] - (error + boost)]
-  # Define particle weights
-  weight <- rep(0L, nrow(.particles))
+  # Define depth likelihoods
+  lik_dc <- rep(0L, nrow(.particles))
   pos <- which(.particles$bathy <= .particles$deep & 
                  .particles$bathy  >= .particles$shallow_1)
-  weight[pos] <- 1L
+  lik_dc[pos] <- 0.99
   pos <- which(.particles$bathy < .particles$shallow_1 & 
                  .particles$bathy >= .particles$shallow_2)
-  weight[pos] <- 0.01
-  weight / sum(weight)
+  lik_dc[pos] <- 0.01
+  # Update likelihoods 
+  lik <- NULL
+  .particles[, lik := lik * lik_dc, ]
+  .particles[lik > 0, ]
 }
 
 #' @title Plot the depth-error model
