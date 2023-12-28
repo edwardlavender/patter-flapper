0a868b9b9fa244a1f2afed949301aed8c222edf2
edwardlavender <el72@st-andrews.ac.uk>
2023-12-28 15:12:43 +0000

    Generalise readRasterLs() and writeRasterLs() functions

--- src/spatial-helpers.R
+++ src/spatial-helpers.R
@@ -1,22 +1,30 @@
 #' @title Spatial helpers
 
-readRasterLs <- function(folder, ...) {
+readRasterLs <- function(folder, ..., index = FALSE) {
   files <- list.files(folder, full.names = TRUE)
   ids   <- tools::file_path_sans_ext(basename(files))
   rasters <- lapply(files, function(file) {
     terra::rast(file, ...)
   })
   names(rasters) <- ids
-  index <- as.character(seq_len(max(as.integer(ids))))
-  lapply(index, function(i) {
-    rasters[[i]]
-  })
+  if (index) {
+    ind <- as.character(seq_len(max(as.integer(ids))))
+    rasters <- lapply(ind, function(i) {
+      rasters[[i]]
+    })
+  }
+  rasters
 }
 
-writeRasterLs <- function(x, folder, ...) {
-  index <- seq_len(length(x))
-  outfiles <- file.path(folder, paste0(index, ".tif"))
-  lapply(index, function(i) {
+writeRasterLs <- function(x, folder, ..., index) {
+  ind <- seq_len(length(x))
+  if (index) {
+    outfiles <- file.path(folder, paste0(ind, ".tif"))
+  } else {
+    stopifnot(!is.null(names(x)))
+    outfiles <- file.path(folder, paste0(names(x), ".tif"))
+  }
+  lapply(ind, function(i) {
     if (!is.null(x[[i]])) {
       terra::writeRaster(x[[i]], outfiles[i], ...)
     }
