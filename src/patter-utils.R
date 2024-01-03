2f6ceb944dde6a91132b85ad90a8895051541d35
edwardlavender <el72@st-andrews.ac.uk>
2024-01-03 17:05:42 +0000

    Include path argument in acs_setup_detection_kernels_read()

--- src/patter-utils.R
+++ src/patter-utils.R
@@ -10,20 +10,20 @@ as_glatos <- function(.acoustics) {
 }
 
 # Read detection kernels
-acs_setup_detection_kernels_read <- function() {
+acs_setup_detection_kernels_read <- function(path = here_data("input", "kernels")) {
   kernels <- list()
   kernels$array_design <- 
-    readRDS(here_data("input", "kernels", "array_design.rds"))
+    readRDS(file.path(path, "array_design.rds"))
   kernels$array_design_by_date <- 
-    readRDS(here_data("input", "kernels", "array_design_by_date.rds"))
+    readRDS(file.path(path, "array_design_by_date.rds"))
   kernels$receiver_specific_kernels <- 
-    readRasterLs(here_data("input", "kernels", "receiver-specific-kernels"))
+    readRasterLs(file.path(path, "receiver-specific-kernels"))
   kernels$receiver_specific_inv_kernels <- 
-    readRasterLs(here_data("input", "kernels", "receiver-specific-inv-kernels"))
+    readRasterLs(file.path(path, "receiver-specific-inv-kernels"))
   kernels$bkg_surface_by_design <- 
-    readRasterLs(here_data("input", "kernels", "bkg-surface-by-design"))
+    readRasterLs(file.path(path, "bkg-surface-by-design"))
   kernels$bkg_inv_surface_by_design <- 
-    readRasterLs(here_data("input", "kernels", "bkg-inv-surface-by-design"))
+    readRasterLs(file.path(path, "bkg-inv-surface-by-design"))
   kernels
 }
 
