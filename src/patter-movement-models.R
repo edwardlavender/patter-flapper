30ac751869cf4b88c77e785065eb184f389e0af4
edwardlavender <el72@st-andrews.ac.uk>
2024-01-09 09:49:35 +0100

    Add uniform rkick and dkick models

--- src/patter-movement-models.R
+++ src/patter-movement-models.R
@@ -7,3 +7,27 @@ rlenuniform <- function(n, .mobility) {
 dlenuniform <- function(x, .mobility) {
   dunif(x, min = 0, max = .mobility)
 }
+
+rkickuniform <- function(.xy0,
+                         .rlen = rlenuniform,
+                         .rang = rangrw,
+                         .obs, .t, .dlist) {
+  # Simulate step lengths and turning angles
+  n    <- nrow(.xy0)
+  rlen <- .rlen(n, .mobility = .obs$mobility[.t])
+  rang <- .rang(n)
+  # Step into new locations
+  cstep(.xy0 = .xy0,
+        .len = rlen,
+        .ang = rang,
+        .lonlat = .dlist$pars$lonlat)
+}
+
+dkickuniform <- function(.xy0, .xy1, .obs, .t, .dlist) {
+  .xy0 <- as.matrix(.xy0)
+  .xy1 <- as.matrix(.xy1)
+  # Calculate step length between selected location and all previous locations
+  rlen <- clen(.xy0 = .xy0, .xy1 = .xy1, .lonlat = .dlist$pars$lonlat)
+  # Calculate densities 
+  dlenuniform(rlen, .mobility = .obs$mobility[.t])
+}
