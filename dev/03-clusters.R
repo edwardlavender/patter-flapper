2f93fdebbedcb8a7ebe9df472fc114a383b3f4cc
edwardlavender <edward.lavender@eawag.ch>
2024-09-22 21:53:32 +0200

    Add comments

--- dev/03-clusters.R
+++ dev/03-clusters.R
@@ -25,8 +25,10 @@ julia_home <- "/Applications/Julia-1.10.app/Contents/Resources/julia/bin/"
 library(patter)
 julia_connect(JULIA_HOME = julia_home)
 
-# Additional R package installation
-# brew install geos
+# (Windows) Additional R package installation
+install.packages("truncdist")
+install.packages("RSP")
+install.packages("spatstat")
 renv::install("rgeos@0.6-4", prompt = FALSE)
 renv::install("rgdal@1.6-7", prompt = FALSE)
 renv::install("maptools@1.1-8", prompt = FALSE)
@@ -36,6 +38,21 @@ devtools::install_github("edwardlavender/prettyGraphics", dependencies = TRUE)
 devtools::install_github("edwardlavender/fvcom.tbx", dependencies = TRUE)
 devtools::install_github("edwardlavender/Tools4ETS", dependencies = TRUE)
 devtools::install_github("edwardlavender/flapper", dependencies = TRUE)
+
+# (USTAN MacBook) Additional R package installation
+# * Install data.table
+# - Install libomp: brew update && brew install libomp
+# - Update .Makevars: 
+# CPPFLAGS += -Xclang -fopenmp
+# LDFLAGS += -lomp
+# - Install: install.packages("data.table", type = "source")
+# * Clone prettyGraphics, move rgeos/rgdal to suggests & install manually
+# * Repeat for Tools4ETS
+# * Repeat for flapper 
+# > We can source the functions from these packages used in this project
+# > which do not require the difficult-to-install geospatial dependencies
+devtools::install_github("edwardlavender/dv", dependencies = TRUE)
+devtools::install_github("edwardlavender/utils.add", dependencies = TRUE)
 
 
 # Checks -----------------------------------------------------------------------
