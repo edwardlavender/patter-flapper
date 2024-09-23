5ef04d00603b637ae87cd1e2147b631a30a4ecf8
edwardlavender <edward.lavender@eawag.ch>
2024-09-23 11:12:03 +0200

    Fix spatstat npixel implementation

---
+++ src/packages-and-options.R
@@ -0,0 +1,34 @@
+# Packages
+library(collapse)
+library(data.table)
+library(dtplyr)
+library(dplyr, warn.conflicts = FALSE)
+library(dv)
+library(JuliaCall)
+library(lubridate)
+library(patter)
+library(spatstat)
+library(tictoc)
+
+# terra colour palette
+op <- options(terra.pal = rev(terrain.colors(256)))
+
+# spatstat options
+# * Use npixel to set the pixel resolution 
+# * It is essential library(spatstat) is called above for this to work properly
+# * For testing, set npixel to small (e.g., 50 pixel)
+# * Otherwise, finer resolution is better
+# * The pixel resolution should just be high enough for the plot to look nice &
+# ... low enough for the calculation not to take too long.
+# See https://stackoverflow.com/questions/67687984/choosing-pixel-size-for-kernel-density-estimate-map-density-ppp-in-r 
+sop <- spatstat.geom::spatstat.options(npixel = 500)
+sop$npixel
+
+# Multi-threading
+data.table::setDTthreads(threads = Sys.getenv("OMP_NUM_THREADS"))
+data.table::getDTthreads()
+
+# Progress bar (chunk) options for cl_lapply()
+# * This is the number of chunks per core
+# * We read rasters into memory on each chunk (slow), so this number is relatively low
+pbo <- pbapply::pboptions("nout" = 10)
