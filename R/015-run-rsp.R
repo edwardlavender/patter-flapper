88a586775386f054b1345ceac81088f32d30afc8
edwardlavender <edward.lavender@eawag.ch>
2024-11-24 20:22:10 +0100

    Check convergence

--- R/015-run-rsp.R
+++ R/015-run-rsp.R
@@ -46,54 +46,73 @@ if (test) {
 }
 
 #### Estimate coordinates
-# Time trial
-lapply_estimate_coord_rsp(iteration = iteration[1, ], datasets = datasets)
-# Implementation (~6 mins)
-lapply_estimate_coord_rsp(iteration = iteration, datasets = datasets)
-# (optional) Examine selected coords
-lapply_qplot_coord(iteration, 
-                  "coord.qs",
-                   extract_coord = function(coord) {
-                     cbind(coord$detections[[1]]$Longitude, 
-                           coord$detections[[1]]$Latitude) |> 
-                       terra::vect(crs = "EPSG:4326") |> 
-                       terra::project("EPSG:32629") |> 
-                       terra::crds() |> 
-                       as.data.frame()
-                   })
+if (FALSE) {
+  # Time trial
+  lapply_estimate_coord_rsp(iteration = iteration[1, ], datasets = datasets)
+  # Implementation (~6 mins)
+  lapply_estimate_coord_rsp(iteration = iteration, datasets = datasets)
+  # (optional) Examine selected coords
+  lapply_qplot_coord(iteration, 
+                     "coord.qs",
+                     extract_coord = function(coord) {
+                       cbind(coord$detections[[1]]$Longitude, 
+                             coord$detections[[1]]$Latitude) |> 
+                         terra::vect(crs = "EPSG:4326") |> 
+                         terra::project("EPSG:32629") |> 
+                         terra::crds() |> 
+                         as.data.frame()
+                     })
+}
 
 #### Estimate UDs
-# Time trial (~25 s)
-lapply_estimate_ud_dbbmm(iteration = iteration[1, ], 
-                         cl = NULL, 
-                         plot = FALSE)
-# Implementation (7 hr, 10 cl)
-lapply_estimate_ud_dbbmm(iteration = iteration, 
-                         cl = 10L, 
-                         plot = FALSE)
-# (optional) Examine selected UDs
-lapply_qplot_ud(iteration, "dbbmm", "ud.tif")
+if (FALSE) {
+  # Time trial (~25 s)
+  lapply_estimate_ud_dbbmm(iteration = iteration[1, ], 
+                           cl = NULL, 
+                           plot = FALSE)
+  # Implementation (7 hr, 10 cl)
+  lapply_estimate_ud_dbbmm(iteration = iteration, 
+                           cl = 10L, 
+                           plot = FALSE)
+  # (optional) Examine selected UDs
+  lapply_qplot_ud(iteration, "dbbmm", "ud.tif")
+}
+
+#### Convergence (100 %)
+ud_data <- lapply(file.path(iteration$folder_ud, "dbbmm", "data.qs"), function(data.qs){
+  qs::qread(data.qs)
+}) |> rbindlist()
+table(ud_data$success)
+# Verify: TRUE
+sapply(file.path(iteration$folder_ud, "dbbmm", "ud.tif"), function(ud.tif){
+  file.exists(ud.tif)
+}) |> table()
 
 #### Mapping (~10 x 3 s)
-unique(iteration$er.ad)
-length(unique(iteration$individual_id))
-length(unique(iteration$month_id))
-mapply(c(500, 250, 750), c("best", "restricted", "flexible"), 
-       FUN = function(param, label) {
-         # Define png args 
-         png_args <- 
-           list(filename = here_fig("analysis", glue("map-rsp-{label}.png")), 
-                height = 10, width = 10, units = "in", res = 600)
-         # Collect data and make figure 
-         iteration |> 
-           filter(er.ad == param) |>
-           mutate(mapfile = file.path(folder_ud, "dbbmm", "ud.tif"), 
-                  individual_id = factor(individual_id, levels = sort(unique(individual_id)))) |>
-           dplyr::select(row = individual_id, column = month_id, mapfile) |>
-           as.data.table() |> 
-           ggplot_maps(png_args = png_args)
-         
-       }) |> invisible()
+if (FALSE) {
+  unique(iteration$er.ad)
+  length(unique(iteration$individual_id))
+  length(unique(iteration$month_id))
+  mapply(c(500, 250, 750), c("best", "restricted", "flexible"), 
+         FUN = function(param, label) {
+           # Define png args 
+           png_args <- 
+             list(filename = here_fig("analysis", glue("map-rsp-{label}.png")), 
+                  height = 10, width = 10, units = "in", res = 600)
+           # Collect data and make figure 
+           iteration |> 
+             filter(er.ad == param) |>
+             mutate(mapfile = file.path(folder_ud, "dbbmm", "ud.tif"), 
+                    individual_id = factor(individual_id, levels = sort(unique(individual_id)))) |>
+             dplyr::select(row = individual_id, column = month_id, mapfile) |>
+             as.data.table() |> 
+             ggplot_maps(png_args = png_args)
+           
+         }) |> invisible()
+}
+
+#### Compute residency 
+# TO DO
 
 
 #### End of code.
