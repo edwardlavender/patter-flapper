1715d706ed17c15ccef3fa49d5a3bd05b5ac939e
edwardlavender <edward.lavender@eawag.ch>
2024-12-10 11:23:31 +0100

    Fix

--- R/010-simulate-algorithms.R
+++ R/010-simulate-algorithms.R
@@ -39,7 +39,7 @@
 rm(list = ls())
 try(pacman::p_unload("all"), silent = TRUE)
 dv::clear()
-Sys.getenv("JULIA_SESSION") == "TRUE"
+Sys.setenv("JULIA_SESSION" = "TRUE")
 
 #### Essential packages
 dv::src()
