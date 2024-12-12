71c0dfbc97ff089f48c7cbadd9d3ba77d474fe89
edwardlavender <edward.lavender@eawag.ch>
2024-12-12 17:47:19 +0100

    Fix incorporation of behaviour

--- src/patter-structures.R
+++ src/patter-structures.R
@@ -1,82 +1,80 @@
-#' ModelMoveFlapperRW
-
-patter_ModelMoveRW <- function(sim) {
-  stopifnot(all(c("k1", "theta1", "k2", "theta2", "mobility") %in% colnames(sim)))
-  move_flapper_rw(mobility = sim$mobility, 
-                  dbn_length_rest = glue::glue("truncated(Cauchy({sim$k1}, {sim$theta1}), lower = 0.0, upper = {sim$mobility})"),
-                  dbn_length_active = glue::glue("truncated(Cauchy({sim$k2}, {sim$theta2}), lower = 0.0, upper = {sim$mobility})"),
-                  dbn_angle = "Uniform(-pi, pi)")
-}
-
-move_flapper_rw <- function(mobility = "1095.0", 
-                            dbn_length_rest = "truncated(Cauchy(0.0, 5.0), lower = 0.0, upper = 1095.0)", 
-                            dbn_length_active = "truncated(Cauchy(5.0, 100.0), lower = 0.0, upper = 1095.0)", 
-                            dbn_angle = "Uniform(-pi, pi)") {
-  julia_check_exists("env")
-  glue("ModelMoveFlapperRW(env, {mobility}, {dbn_length_rest}, {dbn_length_active}, {dbn_angle});")
-}
-
-# Check behaviour of Cauchy distribution matches behaviour in R: yes. 
-# using Plots
-# using Distributions 
-# dist = truncated(Cauchy(0.0, 5.0), lower = 0.0, upper = 1095.0)
-# histogram(rand(dist, 100000), bins = 100)
-# dist = truncated(Cauchy(5.0, 100.0), lower = 0.0, upper = 1095.0)
-# histogram(rand(dist, 100000), bins = 100)
-
-ModelMoveFlapperRW <- 
-  '
-  struct ModelMoveFlapperRW{T, U, V, W, X} <: Patter.ModelMove
-    map::T
-    mobility::U
-    dbn_length_rest::V
-    dbn_length_active::W
-    dbn_angle::X
-  end
-  '
-
-#' simulate_step() method
-# This function assumes that the 'behaviour' vector (1, 2) exists in Julia when sourced
-simulate_step.ModelMoveFlapperRW  <- 
-  '
-  function Patter.simulate_step(state::StateXY, model_move::ModelMoveFlapperRW, t::Int64, behaviour::Vector{Int64} = behaviour)
-  
-    # Simulate step length, depending on behaviour
-    if behaviour[t] == 1
-      length = rand(model_move.dbn_length_rest)
-    elseif behaviour[t] == 2
-      length = rand(model_move.dbn_length_active)
-    else
-      error("behaviour vector must be 1/2.")
-    end 
-    
-    angle     = rand(model_move.dbn_angle)
-    x         = state.x + length * cos(angle)
-    y         = state.y + length * sin(angle)
-    map_value = Patter.extract(model_move.map, x, y)
-    StateXY(map_value, x, y)
-  end 
-'
-
-#' logpdf_step() method
-# This function assumes that the 'behaviour' vector (1, 2) exists in Julia when sourced
-
-logpdf_step.ModelMoveFlapperRW  <- 
-  '
-  function Patter.logpdf_step(state_from::StateXY, state_to::StateXY, model_move::ModelMoveFlapperRW, 
-                       t::Int64, length::Float64, angle::Float64, behaviour::Vector{Int64} = behaviour) 
-    if behaviour[t] == 1
-      length_logpdf = logpdf(model_move.dbn_length_rest, length)
-    elseif behaviour[t] == 2
-      length_logpdf = logpdf(model_move.dbn_length_active, length)
-    end 
-    length_logpdf + logpdf(model_move.dbn_angle, angle)
-  end 
-  '
-
+#' #' ModelMoveFlapperRW
+#' 
+#' patter_ModelMoveRW <- function(sim) {
+#'   stopifnot(all(c("k1", "theta1", "k2", "theta2", "mobility") %in% colnames(sim)))
+#'   move_flapper_rw(mobility = sim$mobility, 
+#'                   dbn_length_rest = glue::glue("truncated(Cauchy({sim$k1}, {sim$theta1}), lower = 0.0, upper = {sim$mobility})"),
+#'                   dbn_length_active = glue::glue("truncated(Cauchy({sim$k2}, {sim$theta2}), lower = 0.0, upper = {sim$mobility})"),
+#'                   dbn_angle = "Uniform(-pi, pi)")
+#' }
+#' 
+#' move_flapper_rw <- function(mobility = "1095.0", 
+#'                             dbn_length_rest = "truncated(Cauchy(0.0, 5.0), lower = 0.0, upper = 1095.0)", 
+#'                             dbn_length_active = "truncated(Cauchy(5.0, 100.0), lower = 0.0, upper = 1095.0)", 
+#'                             dbn_angle = "Uniform(-pi, pi)") {
+#'   julia_check_exists("env")
+#'   glue("ModelMoveFlapperRW(env, {mobility}, {dbn_length_rest}, {dbn_length_active}, {dbn_angle});")
+#' }
+#' 
+#' # Check behaviour of Cauchy distribution matches behaviour in R: yes. 
+#' # using Plots
+#' # using Distributions 
+#' # dist = truncated(Cauchy(0.0, 5.0), lower = 0.0, upper = 1095.0)
+#' # histogram(rand(dist, 100000), bins = 100)
+#' # dist = truncated(Cauchy(5.0, 100.0), lower = 0.0, upper = 1095.0)
+#' # histogram(rand(dist, 100000), bins = 100)
+#' 
+#' ModelMoveFlapperRW <- 
+#'   '
+#'   struct ModelMoveFlapperRW{T, U, V, W, X} <: Patter.ModelMove
+#'     map::T
+#'     mobility::U
+#'     dbn_length_rest::V
+#'     dbn_length_active::W
+#'     dbn_angle::X
+#'   end
+#'   '
+#' 
+#' #' simulate_step() method
+#' # This function assumes that the 'behaviour' vector (1, 2) exists in Julia when sourced
+#' simulate_step.ModelMoveFlapperRW  <- 
+#'   '
+#'   function Patter.simulate_step(state::StateXY, model_move::ModelMoveFlapperRW, t::Int64, behaviour::Vector{Int64} = behaviour)
+#'   
+#'     # Simulate step length, depending on behaviour
+#'     if behaviour[t] == 1
+#'       length = rand(model_move.dbn_length_rest)
+#'     elseif behaviour[t] == 2
+#'       length = rand(model_move.dbn_length_active)
+#'     else
+#'       error("behaviour vector must be 1/2.")
+#'     end 
+#'     
+#'     angle     = rand(model_move.dbn_angle)
+#'     x         = state.x + length * cos(angle)
+#'     y         = state.y + length * sin(angle)
+#'     map_value = Patter.extract(model_move.map, x, y)
+#'     StateXY(map_value, x, y)
+#'   end 
+#' '
+#' 
+#' #' logpdf_step() method
+#' # This function assumes that the 'behaviour' vector (1, 2) exists in Julia when sourced
+#' 
+#' logpdf_step.ModelMoveFlapperRW  <- 
+#'   '
+#'   function Patter.logpdf_step(state_from::StateXY, state_to::StateXY, model_move::ModelMoveFlapperRW, 
+#'                        t::Int64, length::Float64, angle::Float64, behaviour::Vector{Int64} = behaviour) 
+#'     if behaviour[t] == 1
+#'       length_logpdf = logpdf(model_move.dbn_length_rest, length)
+#'     elseif behaviour[t] == 2
+#'       length_logpdf = logpdf(model_move.dbn_length_active, length)
+#'     end 
+#'     length_logpdf + logpdf(model_move.dbn_angle, angle)
+#'   end 
+#'   '
 
 #' ModelMoveFlapperCRW 
-#' (Used to test whether a CRW improves convergence properties)
 
 patter_ModelMoveCRW <- function(sim) {
   stopifnot(all(c("k1", "theta1", "k2", "theta2", "mobility") %in% colnames(sim)))
@@ -101,14 +99,19 @@ struct StateXYD <: Patter.State
     x::Float64
     y::Float64
     angle::Float64  
+    behaviour::Int
 end
   '
 
 states_init.StateXYD <- 
   '
-  function Patter.states_init(state_type::Type{StateXYD}, coords)
+  function Patter.states_init(state_type::Type{StateXYD}, coords, behaviour)
     N = nrow(coords)
+    # Simulate initial coordinates
     coords.angle = rand(N) .* 2 .* π
+    # Set behaviour
+    # (based on change in depth over following time step)
+    coords.behaviour .= behaviour[1]
     return coords
   end 
   '
@@ -128,38 +131,44 @@ simulate_step.ModelMoveFlapperCRW <-
   '
   function Patter.simulate_step(state::StateXYD, model_move::ModelMoveFlapperCRW, t::Int64, behaviour::Vector{Int64} = behaviour)
   
-    # Simulate step length, depending on behaviour
-    if behaviour[t] == 1
-      length = rand(model_move.dbn_length_rest)
-    elseif behaviour[t] == 2
-      length = rand(model_move.dbn_length_active)
-    else
-      error("behaviour vector must be 1/2.")
+    # Simulate step from state (at t - 1), based on behaviour (at time t - 1)
+    # (Behaviour at time t - 1 is based on vertical activity between t - 1 and t)
+    if state.behaviour == 1
+      length  = rand(model_move.dbn_length_rest)
+    elseif state.behaviour == 2
+      length  = rand(model_move.dbn_length_active)
     end 
-    
+    # Simulate angle 
     angle     = state.angle + rand(model_move.dbn_angle_delta)
+    # Update coordinates 
     x         = state.x + length * cos(angle)
     y         = state.y + length * sin(angle)
     map_value = Patter.extract(model_move.map, x, y)
-    StateXYD(map_value, x, y, angle)
+    # Define state at time t
+    # * state at time t includes behaviour at time t
+    # * In logpdf_normalisation_constants(), caching is based on input state (t - 1)
+    # * For each input, we simulate new states to test if they are valid (in water) 
+    # * For caching, t = 1
+    # * Simulated state behaviours are incorrect
+    # * But this does not matter since we only check if states are valid
+    StateXYD(map_value, x, y, angle, behaviour[t])
   end 
 '
 
-# TO DO
 logpdf_step.ModelMoveFlapperCRW <- 
   '
   function Patter.logpdf_step(state_from::StateXYD, state_to::StateXYD, model_move::ModelMoveFlapperCRW, 
                               t::Int64, length::Float64, angle::Float64, behaviour::Vector{Int64} = behaviour) 
                               
     # Compute logpdf_length                          
-    if behaviour[t] == 1
+    if state_from.behaviour == 1
       length_logpdf = logpdf(model_move.dbn_length_rest, length)
-    elseif behaviour[t] == 2
+    elseif state_from.behaviour == 2
       length_logpdf = logpdf(model_move.dbn_length_active, length)
     end 
     
     # Compute logpdf_angle
-    angle_delta = Patter.abs_angle_difference(angle, state_from.angle)
+    angle_delta  = Patter.abs_angle_difference(angle, state_from.angle)
     angle_logpdf = logpdf(model_move.dbn_angle_delta, angle_delta)
     
     # Return (unnnormalised) density of length & turning angle
@@ -179,15 +188,15 @@ logpdf_step.ModelMoveFlapper     <- logpdf_step.ModelMoveFlapperCRW
 #' Helper routine to set initial model components 
 
 set_model_move_components <- function() {
-  # (optional) State State methods for StateXYD (for CRW)
+  # State methods for StateXYD (for CRW)
   JuliaCall::julia_command(StateXYD)
-  JuliaCall::julia_command(states_init.StateXYD)
   # Set movement model (RW or CRW)
   JuliaCall::julia_command(ModelMoveFlapper)
   nothing()
 }
 
 update_model_move_components <- function() {
+  JuliaCall::julia_command(states_init.StateXYD)
   JuliaCall::julia_command(simulate_step.ModelMoveFlapper)
   JuliaCall::julia_command(logpdf_step.ModelMoveFlapper)
   nothing()
