90284b1d057e6bb97c3265448de24a7aa2cd2816
edwardlavender <edward.lavender@eawag.ch>
2024-12-16 12:06:57 +0100

    Reduce batches

--- bin/deploy-simulations.sh
+++ bin/deploy-simulations.sh
@@ -1,55 +1,44 @@
 #!/bin/bash
 
-# Update
-# * Script batch size is too small
-# * Some batches killed (due to memory usage?)
-# * Killed batches re-run (see deploy-simulations-2.sh)
-# * For future runs, run larger batches (fewer CPUs)
+# Record start time
+start_time=$(date)
+echo "Start time: $start_time"
+start_seconds=$(date +%s)
 
-Rscript ./R/010-simulate-algorithms.R 1095.0 "1:10" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "11:20" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "21:30" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "31:40" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "41:50" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "51:60" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "61:70" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "71:80" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "81:90" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "91:100" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "101:110" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "111:120" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "121:130" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "131:140" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "141:150" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "151:160" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "161:170" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "171:180" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "181:190" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "191:200" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "201:210" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "211:220" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "221:230" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "231:240" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "241:250" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "251:260" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "261:270" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "271:280" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "281:290" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "291:300" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "301:310" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "311:320" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "321:330" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "331:340" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "341:350" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "351:360" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "361:370" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "371:380" &
-Rscript ./R/010-simulate-algorithms.R 1095.0 "381:390" &
-Rscript ./R/010-simulate-algorithms.R 985.5 "1:10" &
-Rscript ./R/010-simulate-algorithms.R 985.5 "11:20" &
-Rscript ./R/010-simulate-algorithms.R 985.5 "21:27" &
-Rscript ./R/010-simulate-algorithms.R 1204.5 "1:10" &
-Rscript ./R/010-simulate-algorithms.R 1204.5 "11:20" &
-Rscript ./R/010-simulate-algorithms.R 1204.5 "21:27" &
+# For 1095.0, in batches of 25
+# * The number of batches is relatively low to handle memory requirements
+Rscript ./R/010-simulate-algorithms.R 1095.0 "1:25" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "26:50" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "51:75" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "76:100" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "101:125" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "126:150" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "151:175" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "176:200" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "201:225" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "226:250" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "251:275" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "276:300" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "301:325" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "326:350" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "351:375" &
+Rscript ./R/010-simulate-algorithms.R 1095.0 "376:390" &
+
+# For 985.5, one batch stopping at 27
+Rscript ./R/010-simulate-algorithms.R 985.5 "1:27" &
+
+# For 1204.5, one batch stopping at 27
+Rscript ./R/010-simulate-algorithms.R 1204.5 "1:27" &
+
+# Wait for processes to complete
 wait
-echo "All simulations completed!"
+
+# Print end time & duration (hours, mins, secs)
+end_seconds=$(date +%s)
+duration=$((end_seconds - start_seconds))
+hours=$((duration / 3600))
+minutes=$(((duration % 3600) / 60))
+seconds=$((duration % 60))
+echo "Duration: $hours hours, $minutes minutes, $seconds seconds"
+
+echo " Simulations completed!"
