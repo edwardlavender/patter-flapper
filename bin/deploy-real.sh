dbf93a0d76c8b97720ebf2ea01de0952fa80a607
edwardlavender <edward.lavender@eawag.ch>
2024-12-16 13:28:27 +0100

    Reduce batch size

--- bin/deploy-real.sh
+++ bin/deploy-real.sh
@@ -5,39 +5,28 @@ start_time=$(date)
 echo "Start time: $start_time"
 start_seconds=$(date +%s)
 
-# For 1095.0, in batches of 30, stopping at 528
-Rscript ./R/017-run-patter.R 1095.0 "1:30" &
-Rscript ./R/017-run-patter.R 1095.0 "31:60" &
-Rscript ./R/017-run-patter.R 1095.0 "61:90" &
-Rscript ./R/017-run-patter.R 1095.0 "91:120" &
-Rscript ./R/017-run-patter.R 1095.0 "121:150" &
-Rscript ./R/017-run-patter.R 1095.0 "151:180" &
-Rscript ./R/017-run-patter.R 1095.0 "181:210" &
-Rscript ./R/017-run-patter.R 1095.0 "211:240" &
-Rscript ./R/017-run-patter.R 1095.0 "241:270" &
-Rscript ./R/017-run-patter.R 1095.0 "271:300" &
-Rscript ./R/017-run-patter.R 1095.0 "301:330" &
-Rscript ./R/017-run-patter.R 1095.0 "331:360" &
-Rscript ./R/017-run-patter.R 1095.0 "361:390" &
-Rscript ./R/017-run-patter.R 1095.0 "391:420" &
-Rscript ./R/017-run-patter.R 1095.0 "421:450" &
-Rscript ./R/017-run-patter.R 1095.0 "451:480" &
-Rscript ./R/017-run-patter.R 1095.0 "481:510" &
-Rscript ./R/017-run-patter.R 1095.0 "511:528" &
+# For 1095.0, in batches of 50, stopping at 528
+Rscript ./R/017-run-patter.R 1095.0 "1:50" &
+Rscript ./R/017-run-patter.R 1095.0 "51:100" &
+Rscript ./R/017-run-patter.R 1095.0 "101:150" &
+Rscript ./R/017-run-patter.R 1095.0 "151:200" &
+Rscript ./R/017-run-patter.R 1095.0 "201:250" &
+Rscript ./R/017-run-patter.R 1095.0 "251:300" &
+Rscript ./R/017-run-patter.R 1095.0 "301:350" &
+Rscript ./R/017-run-patter.R 1095.0 "351:400" &
+Rscript ./R/017-run-patter.R 1095.0 "401:450" &
+Rscript ./R/017-run-patter.R 1095.0 "451:500" &
+Rscript ./R/017-run-patter.R 1095.0 "501:528" &
 
-# For 985.5, in batches of 30, stopping at 144
-Rscript ./R/017-run-patter.R 985.5 "1:30" &
-Rscript ./R/017-run-patter.R 985.5 "31:60" &
-Rscript ./R/017-run-patter.R 985.5 "61:90" &
-Rscript ./R/017-run-patter.R 985.5 "91:120" &
-Rscript ./R/017-run-patter.R 985.5 "121:144" &
+# For 985.5, in batches of 50, stopping at 144
+Rscript ./R/017-run-patter.R 985.5 "1:50" &
+Rscript ./R/017-run-patter.R 985.5 "51:100" &
+Rscript ./R/017-run-patter.R 985.5 "101:144" &
 
-# For 1204.5, in batches of 30, stopping at 144
-Rscript ./R/017-run-patter.R 1204.5 "1:30" &
-Rscript ./R/017-run-patter.R 1204.5 "31:60" &
-Rscript ./R/017-run-patter.R 1204.5 "61:90" &
-Rscript ./R/017-run-patter.R 1204.5 "91:120" &
-Rscript ./R/017-run-patter.R 1204.5 "121:144" &
+# For 1204.5, in batches of 50, stopping at 144
+Rscript ./R/017-run-patter.R 1204.5 "1:50" &
+Rscript ./R/017-run-patter.R 1204.5 "51:100" &
+Rscript ./R/017-run-patter.R 1204.5 "101:144" &
 
 # Wait for processes to complete
 wait
